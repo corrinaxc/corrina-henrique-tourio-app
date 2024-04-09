d271ebf8119e3792397b46b15acd167f6ae28592
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-04-09 14:36:46 +0200

    Handle the update data and make the request to the API to save it in the database.

--- pages/places/[id]/edit.js
+++ pages/places/[id]/edit.js
@@ -9,9 +9,15 @@ export default function EditPage() {
   const { isReady } = router;
   const { id } = router.query;
   const { data: place, isLoading, error } = useSWR(`/api/places/${id}`);
-
   async function editPlace(place) {
-    console.log("Place edited (but not really...)");
+    const response = await fetch(`/api/places/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(place),
+    });
+    if (response.ok) {
+      router.back();
+    }
   }
 
   if (!isReady || isLoading || error) return <h2>Loading...</h2>;
@@ -22,7 +28,7 @@ export default function EditPage() {
       <Link href={`/places/${id}`} passHref legacyBehavior>
         <StyledLink justifySelf="start">back</StyledLink>
       </Link>
-      <Form onSubmit={editPlace} formName={'edit-place'} defaultData={place} />
+      <Form onSubmit={editPlace} formName={"edit-place"} defaultData={place} />
     </>
   );
 }
