f186b656d79a1225c2e48a40d453ef7e1b013ecd
Corrina Cornish <corrina.cornish@gmail.com>
2024-04-09 13:23:53 +0200

    POST

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -8,5 +8,14 @@ export default async function handler(request, response) {
     case "GET":
       const places = await Place.find();
       return response.status(200).json(places);
+    case "POST":
+      try {
+        const place = await Place.create(request.body);
+        response.status(200).json({place:place})
+      } catch (error) {
+        response.status(400).json({error:error.message})
+      }
+      break
+
   }
 };
--- pages/create.js
+++ pages/create.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 import Form from "../components/Form.js";
 import { StyledLink } from "../components/StyledLink.js";
+import useSWR from "swr";
 
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
@@ -11,9 +12,18 @@ const StyledBackLink = styled(StyledLink)`
 export default function CreatePlacePage() {
   const router = useRouter();
 
-  function addPlace(place) {
-    console.log("Place added (but not really...)");
-  }
+  async function addPlace(place) {
+    const response = await fetch ("/api/places", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(place)
+    });
+    if (response.ok) {
+      router.push("/")
+    }
+  };
 
   return (
     <>
